34b26d1f01db44fbbff5d41a8184223146bcbc3c
mori928 <mori.nutrition@gmail.com>
2023-08-25 20:55:31 +0900

    DBeaverへのカラム追加２

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must include both letters and numbers" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday true
+  has_many :items
