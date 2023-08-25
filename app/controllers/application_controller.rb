f364644d7f4a4b2c8e76271c03da07b84efd6889
mori928 <mori.nutrition@gmail.com>
2023-08-25 16:09:24 +0900

    ログイン画面１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+   before_action :basic_auth
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
       username == 'furima' && password == '3333'
     end
+  
+    # def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # end
   end
 end
