22b03993cf2e79aa8202c60a8029d8be219311f0
mori928 <mori.nutrition@gmail.com>
2023-08-28 11:20:59 +0900

    ログイン機能　１

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must include both letters and numbers" }
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # has_secure_password
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday true
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   has_many :items
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: :index
+  get "signup", to: "signup#new"
+  post "signup", to: "signup#create"
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
