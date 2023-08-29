924327caddd84ae69a15c3343797dedec9aa8943
mori928 <mori.nutrition@gmail.com>
2023-08-29 19:49:59 +0900

    テストコード　訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-       validates_format_of :password, {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください'} 
-       validates_format_of :family_name, :first_name, {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください'}
-       validates_format_of :family_name_kana, :first_name_kana, {with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'}  
+       validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' 
+       validates_format_of :family_name, with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください'
+       validates_format_of :family_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'
+       validates_format_of :first_name, with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字（漢字・ひらがな・カタカナ）で入力してください'
+       validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'  
       
+
 
        validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   
