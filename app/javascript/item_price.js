4abf2e6cd6ce57cad2d63e90f76ded5d98d17dd0
mori928 <mori.nutrition@gmail.com>
2023-09-01 15:37:44 +0900

    出品登録　最終確認

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,4 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
-
-window.addEventListener('load', () => {
   const priceInput = document.getElementById("price");
   priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
