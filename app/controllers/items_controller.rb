3a30184d5400349e7c96be33258c43fc65bc9e42
mori928 <mori.nutrition@gmail.com>
2023-09-08 19:05:21 +0900

    コードレビュー指摘部分

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :ensure_correct_user, only: [:destroy, :edit]
-
+  
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -30,10 +29,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.nil? || current_user == @item.user || !@item.order.nil?
+    if current_user.nil?
+      redirect_to user_session_path
+    elsif current_user != @item.user || !@item.order.nil?
       redirect_to root_path
     end
   end
+
+
+    # if current_user.nil? || current_user == @item.user || !@item.order.nil?
 
   def update
     if @item.update(item_params)
